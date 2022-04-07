7e258f60f67be516944b6bb913636a893a6d9f83
als-v <alisson.v3@hotmail.com>
2022-04-06 21:07:49 -0300

    adding alteracoes

--- 2. Compilador/2. Análise Sintática/code.tpp
+++ 2. Compilador/2. Análise Sintática/code.tpp
@@ -1,8 +1,18 @@
-inteiro: a
-inteiro: b
-inteiro: c[], c[]
 
-inteiro principal()
-    leia(a)
-    escreva(b)
+inteiro: a
+
+principal()
+
+	inteiro: i
+
+	i := 0
+
+	repita 
+		A[i] := i
+		i := i + 1
+	até i = 20
+
+	leia(n)
+	escreva(busca(n))
+	retorno(0)
 fim
--- 2. Compilador/2. Análise Sintática/parser.py
+++ 2. Compilador/2. Análise Sintática/parser.py
@@ -1,16 +1,16 @@
 import pandas as pd
-table = []
+data = []
 
 with open('saida.txt', 'r') as file:
     for idx, line in enumerate(file):
         
         formatedLine = line.replace('LexToken(', '')
         formatedData = formatedLine.split(',')
-        data = []
 
         if formatedLine[0:7] == 'VIRGULA':
-            data.append([formatedData[0], ',', formatedData[len(formatedData)-2] formatedData[len(formatedData)-1].replace(')', '')])
+            data.append([formatedData[0], ',', formatedData[len(formatedData)-2], formatedData[len(formatedData)-1].replace(')\n', '').replace(')', '')])
         else:
-            formatedData[1] = formatedData[1].replace("'", '')
-            formatedData[3] = formatedData[3].replace(')\n', '')
-            data.append(formatedData)
+            data.append([formatedData[0], formatedData[1].replace("'", ''),formatedData[2], formatedData[3].replace(')\n', '').replace(')', '')])
+
+dataPD = pd.DataFrame(data, columns = ['token', 'valor', 'linha', 'coluna'])
+print(dataPD)
