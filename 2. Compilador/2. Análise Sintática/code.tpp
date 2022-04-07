6e615bb4cf41cdeb65cbc8ad27f165b904c1ce86
als-v <alisson.v3@hotmail.com>
2022-04-07 17:11:32 -0300

    sintatica: alteracoes parser

--- 2. Compilador/2. Análise Sintática/code.tpp
+++ 2. Compilador/2. Análise Sintática/code.tpp
@@ -1,18 +1,2 @@
-
-inteiro: a
-
-principal()
-
-	inteiro: i
-
-	i := 0
-
-	repita 
-		A[i] := i
-		i := i + 1
-	até i = 20
-
-	leia(n)
-	escreva(busca(n))
-	retorno(0)
-fim
+escreva(resultado)
+retorna(0)
--- 2. Compilador/2. Análise Sintática/parser.py
+++ 2. Compilador/2. Análise Sintática/parser.py
@@ -1,4 +1,14 @@
 import pandas as pd
+
+def searchDataLine(data, line):
+    return data.loc[data['linha'] == line]
+
+def searchTokenLineData(data, line, token):
+    lineData = searchDataLine(data, line)
+    lineDataToken = lineData.loc[lineData['token'] == token]
+   
+    return len(lineDataToken) > 0
+    
 data = []
 
 with open('saida.txt', 'r') as file:
@@ -8,9 +18,31 @@ with open('saida.txt', 'r') as file:
         formatedData = formatedLine.split(',')
 
         if formatedLine[0:7] == 'VIRGULA':
-            data.append([formatedData[0], ',', formatedData[len(formatedData)-2], formatedData[len(formatedData)-1].replace(')\n', '').replace(')', '')])
+            data.append([formatedData[0], ',', int(formatedData[len(formatedData)-2]), int(formatedData[len(formatedData)-1].replace(')\n', '').replace(')', ''))])
         else:
-            data.append([formatedData[0], formatedData[1].replace("'", ''),formatedData[2], formatedData[3].replace(')\n', '').replace(')', '')])
+            data.append([formatedData[0], formatedData[1].replace("'", ''), int(formatedData[2]), int(formatedData[3].replace(')\n', '').replace(')', ''))])
 
 dataPD = pd.DataFrame(data, columns = ['token', 'valor', 'linha', 'coluna'])
-print(dataPD)
+
+lineStart = dataPD['linha'].min()
+lineEnd = dataPD['linha'].max()
+print(lineStart, lineEnd)
+
+for line in range(lineStart, lineEnd+1):
+    dataLine = searchDataLine(dataPD, line)
+    # print(dataLine, '\n\n')
+
+    if len(dataLine) > 0:
+
+        if ((searchTokenLineData(dataPD, line, 'ABRE_PARENTESE')) and (not searchTokenLineData(dataPD, line, 'ATRIBUICAO'))):
+            lineIDs = dataLine.loc[dataLine['token'] == 'ID']
+       
+            if len(lineIDs) != 0:
+
+                if len(lineIDs) > 1:
+                    function = lineIDs.iloc[0]
+                else:
+                    function = lineIDs
+
+                print(function, '\n', lineIDs, '\n\n')
+                # print('É uma função:', function['valor'])
